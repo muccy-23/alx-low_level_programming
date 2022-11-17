589d032a8cf416180af489df579e56ed1fd75caf
muccy-23 <muccy23@gmail.com>
2022-11-17 13:20:10 -0800

    this is the first commit for the variadic project

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -19,8 +19,6 @@ typedef struct print
 
 
 int _putchar(char c)
-int _putchar(char);
-
 int sum_them_all(const unsigned int n, ...);
 
 void print_numbers(const char *separator, const unsigned int n, ...);
